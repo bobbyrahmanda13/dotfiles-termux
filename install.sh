6b29ad6ede79cbc900e440116b84d54e6b474afa
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-01-29 12:30:38 +0700

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 termux-change-repo
 termux-setup-storage
-pkg update && pkg upgrade -y
+pkg update -y && pkg upgrade -y
 pkg install bat build-essential ripgrep fd unzip renameutils mediainfo termux-api zoxide eza fzf tmux nano aria2 git wget curl nmap libexpat openssl python ffmpeg python-pip python-pillow rust rust-analyzer starship 7zip neovim nodejs-lts -y
 pip install -U "yt-dlp[default]"
 if [-d ".config"]; then echo "Folder .config sudah ada" else mkdir ".config" echo "Folder .config berhasil dibuat"
