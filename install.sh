910919a083a876d280b45efaf30fdf344652294d
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2025-01-29 10:52:15 +0700

    move folder starship to .config

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ termux-setup-storage
 pkg update && pkg upgrade
 pkg install mediainfo termux-api zoxide eza fzf tmux nano aria2 git wget curl nmap libexpat openssl python ffmpeg python-pip python-pillow rust rust-analyzer starship 7zip neovim nodejs-lts 
 pip install -U "yt-dlp[default]"
+[! -d ".config"] && mkdir ".config"
 [! -d ".shortcuts"] && mkdir ".shortcuts"
 cp -r "${HOME}/dotfiles-termux/.aria2" "${HOME}"
 cp "${HOME}/.aria2/aria2.sh" "${HOME}/.shortcuts"
@@ -11,4 +12,5 @@ cp "${HOME}/dotfiles-termux/youtubemp3/youtubemp3.sh" "${HOME}/.shortcuts"
 chmod +x "${HOME}/.shortcuts/youtubemp3.sh"
 cp "${HOME}/dotfiles-termux/youtubeVideo/youtubeVideo.sh" "${HOME}/.shortcuts"
 chmod +x "${HOME}/.shortcuts/youtubeVideo.sh"
+cp -r "${HOME}/dotfiles-termux/starship" "${HOME}/.config"
 termux-toast "Configuration Completed"
